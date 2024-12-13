96a0c4e4fc36fca7cadd1b372ecb067e00176b69
MyUsername <rosanio.c@gmail.com>
2024-12-13 16:37:49 +0100

    Risoluzione problemi di ASYNC dichiarando i dati dentro il COMPONENT (PROVIDER) invece che sopra l'EXPORT

--- src/contexts/PostsContext.jsx
+++ src/contexts/PostsContext.jsx
@@ -1,64 +1,117 @@
 // # UTILITY
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 
 // # CONTEXT VARIABLE
 const PostsContext = createContext();
 
 // # CONTEXT DATA
 // E' BEST-PRACTICE utilizzare un OBJECT come dati del CONTEXT da inserire nel PROVIDER
-const postsData = {
-    postsList: [
-        {
-            id: 2,
-            category: 'Recipes',
-            title: 'Ciambellone',
-            content: 'Delizioso ciambellone',
-            img: `ciambellone.jpeg`,
-            tags: ['ciambellone', 'Dolce', 'Torte'],
-            published: true,
-        },
-        {
-            id: 4,
-            category: 'Recipes',
-            title: 'Cracker di barbabietola',
-            content: 'Preparazione cracker di barbabietola',
-            img: `cracker_barbabietola.jpeg`,
-            tags: ['barbabietola', 'Cracker', 'impasto', 'salato'],
-            published: true,
-        },
-        {
-            id: 50,
-            category: 'Recipes',
-            title: 'Pane fritto',
-            content: 'Come fare il pane fritto',
-            img: `pane_fritto_dolce.jpeg`,
-            tags: ['dolce', 'Pane fritto'],
-            published: true,
-        },
-        {
-            id: 8,
-            category: 'Recipes',
-            title: 'Pasta di barbabietola',
-            content: 'Come preparare la pasta alla barbabietola',
-            img: `pasta_barbabietola.jpeg`,
-            tags: ['barbabietola', 'pasta', 'impasto', 'salato'],
-            published: true,
-        },
-        {
-            id: 10,
-            category: 'Recipes',
-            title: 'Torta paesana',
-            content: 'La torta paesana più buona',
-            img: `torta_paesana.jpeg`,
-            tags: ['torte', 'torta paesana', 'dolce'],
-            published: true,
-        }
-    ],
-}
+// const postsData = {
+//     postsList: [
+//         {
+//             id: 2,
+//             category: 'Recipes',
+//             title: 'Ciambellone',
+//             content: 'Delizioso ciambellone',
+//             img: `ciambellone.jpeg`,
+//             tags: ['ciambellone', 'Dolce', 'Torte'],
+//             published: true,
+//         },
+//         {
+//             id: 4,
+//             category: 'Recipes',
+//             title: 'Cracker di barbabietola',
+//             content: 'Preparazione cracker di barbabietola',
+//             img: `cracker_barbabietola.jpeg`,
+//             tags: ['barbabietola', 'Cracker', 'impasto', 'salato'],
+//             published: true,
+//         },
+//         {
+//             id: 50,
+//             category: 'Recipes',
+//             title: 'Pane fritto',
+//             content: 'Come fare il pane fritto',
+//             img: `pane_fritto_dolce.jpeg`,
+//             tags: ['dolce', 'Pane fritto'],
+//             published: true,
+//         },
+//         {
+//             id: 8,
+//             category: 'Recipes',
+//             title: 'Pasta di barbabietola',
+//             content: 'Come preparare la pasta alla barbabietola',
+//             img: `pasta_barbabietola.jpeg`,
+//             tags: ['barbabietola', 'pasta', 'impasto', 'salato'],
+//             published: true,
+//         },
+//         {
+//             id: 10,
+//             category: 'Recipes',
+//             title: 'Torta paesana',
+//             content: 'La torta paesana più buona',
+//             img: `torta_paesana.jpeg`,
+//             tags: ['torte', 'torta paesana', 'dolce'],
+//             published: true,
+//         }
+//     ],
+// }
 
 // # PROVIDER EXPORT
 // Il provider "abbraccerà" tutti i suoi CHILDREN, a cui verranno resi disponibili i dati del VALUE
 export const PostsContextProvider = ({ children }) => {
+
+    // IMPORT DATA (interno al COMPONENT)
+    // E' fondamentale che i dati siano importati all'INTERNO del COMPONENT per evitare errori di ASYNC. Importandoli sopra, i dati potrebbero NON essere caricati al MOUNTING del COMPONENT
+    const postsData = {
+        postsList: [
+            {
+                id: 2,
+                category: 'Recipes',
+                title: 'Ciambellone',
+                content: 'Delizioso ciambellone',
+                img: `ciambellone.jpeg`,
+                tags: ['ciambellone', 'Dolce', 'Torte'],
+                published: true,
+            },
+            {
+                id: 4,
+                category: 'Recipes',
+                title: 'Cracker di barbabietola',
+                content: 'Preparazione cracker di barbabietola',
+                img: `cracker_barbabietola.jpeg`,
+                tags: ['barbabietola', 'Cracker', 'impasto', 'salato'],
+                published: true,
+            },
+            {
+                id: 50,
+                category: 'Recipes',
+                title: 'Pane fritto',
+                content: 'Come fare il pane fritto',
+                img: `pane_fritto_dolce.jpeg`,
+                tags: ['dolce', 'Pane fritto'],
+                published: true,
+            },
+            {
+                id: 8,
+                category: 'Recipes',
+                title: 'Pasta di barbabietola',
+                content: 'Come preparare la pasta alla barbabietola',
+                img: `pasta_barbabietola.jpeg`,
+                tags: ['barbabietola', 'pasta', 'impasto', 'salato'],
+                published: true,
+            },
+            {
+                id: 10,
+                category: 'Recipes',
+                title: 'Torta paesana',
+                content: 'La torta paesana più buona',
+                img: `torta_paesana.jpeg`,
+                tags: ['torte', 'torta paesana', 'dolce'],
+                published: true,
+            }
+        ],
+    }
+
     return <>
         <PostsContext.Provider value={postsData}>{children}</PostsContext.Provider>
     </>
